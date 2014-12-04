776fce3a9a8d305612ba32c5867e91ec420faf89
amidupeuple <danyapivovarov@gmail.com>
2014-12-04 08:55:59 +0300

    Now it can play music

--- src/com/github/amidupeuple/main/MainActivity.java
+++ src/com/github/amidupeuple/main/MainActivity.java
@@ -13,12 +13,39 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ListView;
 import com.github.amidupeuple.model.Song;
+import android.os.IBinder;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
     private ArrayList<Song> songList;
     private ListView songView;
+
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound = false;
+
+    private ServiceConnection musicConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder) iBinder;
+            musicSrv = binder.getService();
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            musicBound = false;
+        }
+    };
+
     /**
      * Called when the activity is first created.
      */
@@ -40,9 +67,11 @@ public class MainActivity extends Activity {
 
         SongAdapter songAdapter = new SongAdapter(this, songList);
         songView.setAdapter(songAdapter);
+
+
     }
 
-    public void getSongList() {
+    private void getSongList() {
         ContentResolver musicResolver = getContentResolver();
         Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
@@ -60,5 +89,41 @@ public class MainActivity extends Activity {
                 Log.d(TAG, "New song added: " + thisArtist + ":" + thisTitle);
             } while (musicCursor.moveToNext());
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (playIntent == null) {
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            //startService(playIntent);
+        }
+    }
+
+    public void songPicked(View view) {
+        musicSrv.setSong((Integer.parseInt(view.getTag().toString())));
+        musicSrv.playSong();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_shuffle:
+                break;
+            case R.id.action_end:
+                stopService(playIntent);
+                musicSrv = null;
+                System.exit(0);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(playIntent);
+        musicSrv = null;
+        super.onDestroy();
     }
 }
--- src/com/github/amidupeuple/main/MusicService.java
+++ src/com/github/amidupeuple/main/MusicService.java
@@ -12,6 +12,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.PowerManager;
+import android.provider.MediaStore;
 import android.util.Log;
 import com.github.amidupeuple.model.Song;
 
@@ -20,6 +21,8 @@ import com.github.amidupeuple.model.Song;
  */
 public class MusicService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
         MediaPlayer.OnCompletionListener {
+    private static final String TAG = "MusicService";
+
     //media player
     private MediaPlayer player;
     //song list
@@ -27,9 +30,18 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     //current position
     private int songPosn;
 
+    private final IBinder musicBind = new MusicBinder();
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        player.stop();
+        player.release();
+        return false;
     }
 
     @Override
@@ -44,6 +56,51 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
 
     @Override
     public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        songPosn = 0;
+        player = new MediaPlayer();
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer() {
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    public  void setList(ArrayList<Song> theSongs) {
+        songs = theSongs;
+    }
+
+    public class MusicBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    public void playSong(){
+        player.reset();
+        Song playSong = songs.get(songPosn);
+        long currSong = playSong.getId();
+        Uri trackUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, currSong);
+
+        try {
+            player.setDataSource(getApplicationContext(), trackUri);
+        } catch (Exception e) {
+            Log.e(TAG, "Error setting data source", e);
+        }
+
+        player.prepareAsync();
+    }
+
+    public void setSong(int songIndex) {
+        songPosn = songIndex;
     }
 }
